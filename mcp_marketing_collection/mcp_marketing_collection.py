d7d9884a6a48bc988353317405acfcca2e1d9ea6
IdeaBosque <ideabosque@gmail.com>
2026-03-27 20:02:05 -0700

    Fix a bug related to place is none.

--- mcp_marketing_collection/mcp_marketing_collection.py
+++ mcp_marketing_collection/mcp_marketing_collection.py
@@ -375,8 +375,11 @@ class MCPMarketingCollection:
                     [
                         contact_profile.get("first_name") == contact.get("first_name"),
                         contact_profile.get("last_name") == contact.get("last_name"),
-                        contact_profile.get("place", {}).get("place_uuid")
-                        == place.get("place_uuid"),
+                        (
+                            contact_profile.get("place") is not None
+                            and contact_profile.get("place", {}).get("place_uuid")
+                            == place.get("place_uuid")
+                        ),
                     ]
                 ):
                     return contact_profile
